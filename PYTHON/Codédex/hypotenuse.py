36e4187d5d268788e366221d7aba8ef7ae4c2896
Túlio Thauã Dutra <tuliothauandutra@gmail.com>
2025-10-27 19:01:16 +0000

    feat: add program that calculates hypotenuse

---
+++ PYTHON/Codédex/hypotenuse.py
@@ -0,0 +1,12 @@
+# This Program Calculates the Hypotenuse!
+
+#[VAR] :: Creating the Entry!
+a = int(input('Set a value to A: '))
+b = int(input('Set a value to B: '))
+
+#[FUNC] :: Calculating the Hypotenuse!
+
+c = (a**2 + b**2) ** 0.5
+
+#[EXEC] :: Spittin on the Screen!
+print(c)
