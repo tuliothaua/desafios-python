d1cb69bfa9af020f8d413b2a959b89608899477f
tuliothaua <tuliothauadutra@uniplaclages.edu.br>
2025-10-29 20:18:01 -0300

    feat: add grades program

---
+++ PYTHON/Codédex/grades.py
@@ -0,0 +1,10 @@
+# This Program Calculates your Grades!
+
+#[VAR] :: Creating the Variables!
+grade = 80
+
+#[FUNC] :: Checking if you passed or failed!
+if grade >= 55:
+  print('You passed')
+else:
+  print('You failed')
