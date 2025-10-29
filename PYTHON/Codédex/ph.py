243b477bb24ae1f234a247f4d8dc191b678620ee
tuliothaua <tuliothauadutra@uniplaclages.edu.br>
2025-10-29 20:36:06 -0300

    feat: add ph chekcer program

---
+++ PYTHON/Codédex/ph.py
@@ -0,0 +1,12 @@
+# This Program Checks the pH Level!
+
+#[VAR] :: Creating the Variables!
+ph = 10
+
+#[FUNC] :: Checking the pH Level!
+if ph > 7:
+  print("Basic")
+elif ph < 7:
+  print("Acidic")
+else:
+  print("Neutral")
